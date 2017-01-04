ed4e8aae8445ba457a74e4d496d6df128784a07d
adrianmoric <adrian_moric@yahoo.com>
2017-01-04 18:28:30 +0100

    Add files via upload

---
+++ BPCasopis.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Data.Sqlite;
+
+
+
+
+namespace ProjektProgramsko
+{
+	public static class BPCasopis
+	{
+		public static void Spremi(Casopis k)
+		{
+			BP.otvoriKonekciju();
+
+			SqliteCommand command = BP.komanda();
+			command.CommandText = @"create table if not exist Casopis (
+
+			id integer primary key autoincrement,
+			naziv string,
+			tagovi string)";
+
+		}
+
+
+		public static List<Casopis> DohvatiSve()
+		{
+			List<Casopis> listaCasopisa = new List<Casopis>();
+
+			BP.otvoriKonekciju();
+
+			SqliteCommand command = BP.komanda();
+
+			command.CommandText = "Select * from casopis, sadrzaj where casopis.id_sadrzaj = sadrzaj.id";
+
+			SqliteDataReader reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				Casopis k = new Casopis();
+
+				k.Id = (int)(Int64)reader["id"];
+				k.Naziv = (string)reader["naziv"];
+				k.Tagovi = (string)reader["tagovi"];
+
+				listaCasopisa.Add(k);
+			}
+
+			reader.Dispose();
+			command.Dispose();
+
+			return listaCasopisa;
+		}
+	}
+}
+
---
+++ BPFilm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using Mono.Data.Sqlite;
+
+
+namespace ProjektProgramsko
+{
+	public static class BPFilm
+	{
+		public static void Spremi(Film k)
+		{
+			BP.otvoriKonekciju();
+
+			SqliteCommand command = BP.komanda();
+			command.CommandText = @"create table if not exist film (
+
+			id integer primary key autoincrement,
+			godina DateTime,
+			naziv string,
+			trajanje integer,
+			cijena double,
+			tagovi string,
+			opis string)"; 
+
+		}
+
+
+
+		public static List<Knjiga> DohvatiSve()
+		{
+			List<Film> listaFilmova = new List<Knjiga>();
+
+			BP.otvoriKonekciju();
+
+			SqliteCommand command = BP.komanda();
+
+			command.CommandText = "Select * from film, sadrzaj where film.id_sadrzaj = sadrzaj.id";
+
+			SqliteDataReader reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				Film k = new Film();
+
+				k.Id = (int)(Int64)reader["id"];
+				k.Naziv = (string)reader["naziv"];
+				k.Tagovi = (string)reader["tagovi"];
+
+				listaFilmova.Add(k);
+			}
+
+			reader.Dispose();
+			command.Dispose();
+
+			return listaFilmova;
+	}
+}
+
